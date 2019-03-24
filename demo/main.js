97542fbfad0e4d289f79c71ce2c399726959ccc6
aniruddha <aniruddha.kudalkar@gmail.com>
2019-03-24 11:26:28 +0530

    demo : codes for demo lecture

---
+++ demo/main.js
@@ -0,0 +1,9 @@
+console.log(console)
+console.log(document)
+console.log(document.getElementById('android'))
+document.getElementById('android').style.border = '1px solid red'
+
+document.getElementById()
+function popDv(val, dv){ 
+   dv.innerHTML = val
+}
