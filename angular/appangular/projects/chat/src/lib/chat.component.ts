240a6c8755bc98ad3c7846086fa1cd2c16d2970c
aniruddha <aniruddha.kudalkar@gmail.com>
2019-04-06 10:00:39 +0530

    workspace :  application and library

---
+++ angular/appangular/projects/chat/src/lib/chat.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-chat',
+  template: `
+    <p>
+      chat works!
+    </p>
+  `,
+  styles: []
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
