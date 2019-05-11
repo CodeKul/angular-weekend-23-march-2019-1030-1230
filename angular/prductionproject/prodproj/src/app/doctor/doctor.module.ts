3bb1de9ae11173e3bb4600021e5b5760ed6e831a
aniruddha <aniruddha.kudalkar@gmail.com>
2019-05-11 12:56:49 +0530

    modules : third party modules

--- angular/prductionproject/prodproj/src/app/doctor/doctor.module.ts
+++ angular/prductionproject/prodproj/src/app/doctor/doctor.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoctorComponent } from './doctor/doctor.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @NgModule({
   declarations: [DoctorComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    NgbModule,
+    Ng2SmartTableModule
+  ], exports: [DoctorComponent]
 })
 export class DoctorModule { }
--- angular/prductionproject/prodproj/src/app/doctor/doctor/doctor.component.ts
+++ angular/prductionproject/prodproj/src/app/doctor/doctor/doctor.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoctorComponent implements OnInit {
 
+  prg : number = 0
+  settings = {
+    columns: {
+      id: {
+        title: 'ID'
+      },
+      name: {
+        title: 'Full Name'
+      },
+      username: {
+        title: 'User Name'
+      },
+      email: {
+        title: 'Email'
+      }
+    }
+  };
+
+  data = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz"
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv"
+    },
+    
+    {
+      id: 11,
+      name: "Nicholas DuBuque",
+      username: "Nicholas.Stanton",
+      email: "Rey.Padberg@rosamond.biz"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  clickMe() {
+    ++this.prg
+  }
 }
