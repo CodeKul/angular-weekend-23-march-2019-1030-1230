c3e559078918589eaa675845824fa89e50407e26
aniruddha <aniruddha.kudalkar@gmail.com>
2019-05-18 10:20:11 +0530

    forms : DD controls and form submission

--- angular/prductionproject/prodproj/projects/forms/src/app/dd/dd.component.ts
+++ angular/prductionproject/prodproj/projects/forms/src/app/dd/dd.component.ts
@@ -17,8 +17,14 @@ export class DdComponent implements OnInit {
 
   ngOnInit() {
     this.frmGrp = this.fb.group({
-
+      usNm : this.fb.control(''),
+      eml : this.fb.control(''),
+      pass : this.fb.control(''),
     })
+    console.log(this.frmGrp)
+  }
+
+  onSub() {
     console.log(this.frmGrp)
   }
 }
